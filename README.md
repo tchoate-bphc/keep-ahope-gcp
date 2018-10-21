c48716dec612ab44718874595f839120b17f294e
JL <jie.lu@ge.com>
2018-10-21 18:15:32 -0400

    jlu 10/21/18: migrating code form our gitlab repository.

--- README.md
+++ README.md
@@ -1,5 +1,62 @@
-# keep-ahope-gcp
-Backend and service layers built on Google Cloud Platform
+# AHOPE Project on Google App Engine:  Postgresql, Parse-Server, and Parse-dashboard in backend with Google Sign-in for Authentication
+
+This project sets up [Parse-server](https://github.com/ParsePlatform/parse-server) and [Parse-dashboard](https://github.com/parse-community/parse-dashboard) to run 
+[Google App Engine](https://cloud.google.com/appengine) Node.js [standard environment](https://cloud.google.com/appengine/docs/standard/nodejs).  This project is based on the original [Parse sample project](https://github.com/GoogleCloudPlatform/nodejs-docs-samples/tree/master/appengine/parse-server)
+
+## Downloading Files
+
+1. `git clone https://github.com/evandana/keep-ahope-gcp`
+1. `cd keep-ahope-gcp`
+
+## Setup -  Cloud (App Engine) Only
+
+1. Create a project in the [Google Cloud Platform Console](https://console.cloud.google.com/).
+1. [Enable billing](https://console.cloud.google.com/project/_/settings) for your project.
+1. [Create an OAuth2 client](https://console.cloud.google.com/apis/credentials) for your project.  This is done by choosing "Create credentials" then "OAuth client ID" under the Credentials tab.  Select "Web Application" as the Application type.  The name of the client doesn't matter, but be sure to add to "Authorized JavaScript origins" the domain (in the format of scheme://server-address:port-number) where the app will run.  Note the client id that is created.
+1. Install the [Google Cloud SDK](https://cloud.google.com/sdk/).
+1. Create a Cloud SQL instance (Postgresql flavor) in the same project, and note its cloud SQL instance connection name, such as:
+   `ahope-parse-poc:northamerica-northeast1:ahope-poc-db`
+   It is simple concatenation of GCloud project name, region name, and the CloudSQL instance id, separated by colons.
+1. In the Cloud SQL instance, create a database user (also known as a "login role" in Postgresql) and create a database that this user either owns
+   or has all privileges.
+1. In [config.yml](./config.yml) file, configure properties as documented there.
+1. As of now, you also need to add the CloudSQL connection name (see above) to app.standard.yml, under beta_settings.cloud_sql_instances
+
+## Setup - Local
+
+1. Setup a Postgresql server either locally or on another host that the local dev machine can reach by TCP socket.  Cloud SQL uses Postgresql 9.6.  While it is recommended to use the same version when installing locally, it's unlikely for version to matter if you want to use an existing Postgresql server of a version that is close to 9.6.  Collect the hostname and port number.
+2. Same as setting up for cloud, create a database user and a database that this new user owns or can administer.
+3. In [config-dev.yml](./config-dev.yml) file, create/update the following properties:
+    1. DATABASE_URI: "postgres://\<db-user>:\<db-password>@/\<hostname:port>/\<database-name>".
+    1. SERVER_URL: "http://localhost:8080/parse"  
+    Note that all properties in [config.yml](./config.yml) file are inherited, subject to overriding.  
+    
+    If you want to run Parse server locally but using the Cloud SQL in Google Cloud, you can install and run
+    a proxy, which opens and listens on a local TCP port to receive and route Postgresql requests to the Cloud SQL instance.  You should update DATABASE_URI
+    accordingly to reflect the local host name and the port number used by the proxy.  For more details see [Cloud SQL Proxy](https://cloud.google.com/sql/docs/postgres/sql-proxy).
+
+## Running locally
+
+1. Follow the "Setup - Local" section above.  Make sure the configuration files are set correctly.
+1. `npm install`
+1. `npm start`
+1. Goto http://localhost:8080/dashboard to access the Parse Dashboard.  URL http://localhost:8080/parse can be used for REST API access.
+1. When logging into dashboard running locally, use ahope-admin/ahope as username/password.  See [server.js](./server.js) where passwords are set for local and cloud environments.
+1. Note that Parse Server, when running locally but behind a corporate firewall, will likely not be able to validate an id token provided by Google Sign-in.
+
+## Client side development
+
+1. See a [sample page](./static/index.html) for how Google Sign-in and Parse Javascript SDK are used.
+1. To access the page locally, go to http://localhost:8080/static/index.html.
+1. Any complex business logic can be implemented as "Cloud Functions", as shown in [Cloud Function](./cloud/main.js), which runs on the server side.
+
+## Deploy to App Engine standard environment
+
+1. Make sure the necessary [environment variables](./config.yml) are set correctly.  See the "Setup - Cloud (App Engine) Only" section above.
+1. `gcloud app deploy app.standard.yaml`
+
+Refer to the [appengine/README.md](https://github.com/GoogleCloudPlatform/nodejs-docs-samples/tree/master/appengine/README.md) file for more instructions on
+running and deploying.
 
 ## UI
 
---
+++ server.js
@@ -0,0 +1,139 @@
+/**
+ * Copyright 2016, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// [START app]
+const express = require('express');
+const nconf = require('nconf');
+const ParseServer = require('parse-server').ParseServer;
+const path = require('path');
+const yml = require('js-yaml');
+const ymlFormatter = { parse: yml.safeLoad, stringify: yml.safeDump };
+
+var projectId, connectionName, databaseName;
+
+if (process.env.NODE_ENV === 'production') {
+  require('@google-cloud/debug-agent').start();
+}
+
+nconf.argv().env()
+  .file('dev-override', {file: 'config-dev.yml', format: ymlFormatter})
+  .file({ file: 'config.yml', format: ymlFormatter });
+
+const app = express();
+
+// Reference: https://parseplatform.org/parse-server/api/master/ParseServerOptions.html
+const serverConfig = {
+  cloud: nconf.get('CLOUD_PATH') || path.join(__dirname, '/cloud/main.js'),
+  // This appId is used to uniquely identify one out of many possible "applications" hosted by one Parse server.
+  // we are only deploying one app.
+  appId: nconf.get('APP_ID') || 'AHOPE-App-in-Parse',
+  // An empty or undefined master key indicates that Parse server and Parse Dashboard are collocated and can use a randomly generated password
+  masterKey: nconf.get('MASTER_KEY') || require('crypto').randomBytes(48).toString('base64'),
+  fileKey: nconf.get('FILE_KEY'),
+  mountPath: nconf.get('PARSE_MOUNT_PATH') || '/parse',
+  auth: { google: true },
+  enableAnonymousUsers: false,
+  allowClientClassCreation: nconf.get('ALLOW_CLIENT_CLASS_CREATION') || false
+}
+
+
+serverConfig.serverURL = nconf.get('SERVER_URL') || 'https://'.concat(projectId = nconf.get('PROJECT_ID'), '.appspot.com', serverConfig.mountPath),
+
+
+serverConfig.databaseURI = nconf.get('DATABASE_URI');
+
+function getConnectionName()
+{
+  return nconf.get('CLOUDSQL_CONNECTION_NAME') || 
+    [(projectId || nconf.get('PROJECT_ID')), nconf.get('CLOUDSQL_REGION'), nconf.get('CLOUDSQL_INSTANCE_ID')].join(':')
+}
+
+if (!serverConfig.databaseURI) {
+  if (!connectionName) connectionName = getConnectionName();
+  const databaseCredentials = nconf.get('DATABASE_CREDENTIALS');
+  databaseName = nconf.get('DATABASE_NAME');
+  serverConfig.databaseURI = 'postgres://'.concat(databaseCredentials, '@/cloudsql/', connectionName, '/', databaseName);
+}
+
+
+serverConfig.databaseOptions = nconf.get('DATABASE_OPTIONS');
+
+// If DATABASE_OPTIONS is not defined at all, then populate it.
+// If DATABASE_OPTIONS is specifically defined to be empty, then leave it as is - this is useful in local dev mode,
+// where we don't need any database options beyond what is provided in serverConfig.databaseURI.
+if (!serverConfig.databaseOptions) {
+  // In Google Cloud, in order for an App to "locally" access a CloudSQL database, UNIX domain socket has to be used to connect.
+  // Hence the need to provide a databaseOptions.  It appears that if we provide value for .protocol, then we need to provide
+  // .host and .database as well, as their values may not be picked up from serverConfig.databaseURI.
+  //
+  serverConfig.databaseOptions = 
+  {
+    protocol: 'socket:',
+    host: '/cloudsql/'.concat(connectionName || getConnectionName()),
+    database: databaseName || nconf.get('DATABASE_NAME')
+  };
+}
+
+// oauthClientId is to be passed onto the client side to allow user authentication using Google sign-in.
+const oauthClientId = nconf.get('OAUTH_CLIENT_ID');
+
+console.log("OAuth Client ID is: " + oauthClientId);
+console.log("database URI is: " + serverConfig.databaseURI);
+console.log("databaseOptions is: " + JSON.stringify(serverConfig.databaseOptions));
+
+const parseServer = new ParseServer(serverConfig);
+
+// Mount the Parse API server middleware to /parse
+app.use(serverConfig.mountPath || '/parse', parseServer);
+
+const dashboardSettings = nconf.get('DASHBOARD_SETTINGS');
+
+if (dashboardSettings) {
+  if (!dashboardSettings.apps) dashboardSettings.apps = [{}];
+
+  const appSettings = dashboardSettings.apps[0];
+
+  if (!appSettings.serverURL) appSettings.serverURL = serverConfig.serverURL;
+  if (!appSettings.appId) appSettings.appId = serverConfig.appId;
+  if (!appSettings.masterKey) appSettings.masterKey = serverConfig.masterKey;
+  if (!appSettings.appName) appSettings.appName = "AHOPE Administrators Dashboard";
+
+  console.log("Server URL used by dashboard is: " +  appSettings.serverURL);
+
+  dashboardSettings.users.forEach(user => { if (!user.apps) user.apps = [{appId: serverConfig.appId}] } ) ;
+
+  const ParseDashboard = require('parse-dashboard');
+  const dashboard = new ParseDashboard(dashboardSettings,  { allowInsecureHTTP: true });
+  app.use('/dashboard', dashboard); 
+}
+
+app.get('/', (req, res) => {
+  if (dashboardSettings) {
+    res.redirect('/dashboard');
+  } else {
+    res.status(200).send('Hello, world!');
+  }  
+});
+
+app.use('/static', express.static('static'))
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
