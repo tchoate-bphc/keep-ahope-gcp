35b2130c034bbff27ce51628de4e79e7bdba1a98
pandathegiant <jie.lu@ge.com>
2019-02-10 13:19:33 -0500

    As a part of database initialization, create roles and set class level permissions (#8)
    This PR includes the code in the back-end to do the following when the server starts up:
    
    1. creates a few "built-in" roles (Viewer and Editor primarily) in Parse if they do not exist yet,
    1. configures nested role membership, and
    1. uses the roles to set class-level permissions on event and contacts classes.
    
    In light of issues arising from these changes, as detailed in https://github.com/evandana/keep-ahope/issues/116 and https://github.com/evandana/keep-ahope/issues/117, the following subsequent changes have been added in recent commits:
    
    1. Master key generation: each service instance generating its own master key caused "invalid master key" errors, when a request sent by Dashboard in one instance is routed to the Parse server in another.  Therefore, the key generation algorithm has been updated to use random numbers generated at build time, and hence is version-specific.
    
    2. Dashboard will now connect to the same version of the service that it itself is from, hence guaranteeing it has the right master key.
    
    3. Invocation of schema initialization has also been updated significantly:
    
        - Use of the public URL of the Parse server to perform schema initialization at instance start-up time caused a spike in REST/http requests that in turn caused more service instances to start, as GCP attempted to scale up to meet the load, further aggravating the problem.  It has been updated to use localhost (since there is a Parse server in each instance), which eliminates unnecessary scaling-up **AND** invalid master key errors (for schema initialization).
    
        - SQL errors could still happen if each of many instances at starting time still performs schema initialization.  In a separate Pull Request that has been merged, a new endpoint was created to perform initialization only *upon requests*.  This PR further protects this endpoint with a password determined at build time, so to ward off unauthorized/illicit requests to initialize schema.
    
        - GCP cloud build configuration is updated so that schema initialization is initiated as the last step of its build process.  A random password is generated and is used to call the schema initialization endpoint.

--- parseSchema/index.js
+++ parseSchema/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const Parse = require('parse/node');
+const axios = require('axios');
 
 /**
  * This function compares the fields in schemaFieldsToAdd against the existing schema, excludes any existing fields from the former,
@@ -15,25 +16,142 @@ function addAnyNewFieldsToSchema(existingSchemaData, schemaFieldsToAdd)
     schemaFieldsToAdd.update() : Promise.resolve(existingSchemaData);
 }
 
-function setParseSchema (serverConfig) {
+/**
+ * This function creates a Role if it doesn't exist yet, and then optionally add other Roles as its members if those others are not yet a member.
+ * @param {String} roleName name of the Role to be created if absent.
+ * @param {Parse.Role} members collection of other Roles that automatically assumes this Role.
+ * @return {Promise} A Promise, resolvable to the newly created or retrieved existing role.
+ * @private
+ */
+function createRole(roleName, ...members)
+{
+  // By specifying no write privileges for the ACL, we can ensure the role cannot be altered.
+  const roleAcl = new Parse.ACL();
+  roleAcl.setPublicReadAccess(true);
+
+  const options = {"useMasterKey": true};
+
+  // Note:
+  // Despite what the sample code here (https://docs.parseplatform.org/js/guide/#retrieving-objects) might imply,
+  // can't use Parse.Object.extend("Role") in place of Parse.Role here to query roles.
+  return (new Parse.Query(Parse.Role))
+    .equalTo("name", roleName)
+    .first(options)
+    .then(role => (role !== undefined) ? Promise.resolve(role) : (new Parse.Role(roleName, roleAcl)).save(null, options))
+    .then(role => {
+      if (members.length > 0) {
+        var roles = role.getRoles();
+        members.forEach(member => roles.add(member));
+        return role.save(null, options);
+      }
+      return Promise.resolve(role);
+    });
+}
+
+/**
+ * This function sets for a class such Class Level Permissions: Writer role can write, and Viewer can read.
+ * 
+ * See the example here:
+ * https://docs.parseplatform.org/rest/guide/#requires-authentication-permission-requires-parse-server---230, and 
+ * somewhat contradictory: https://docs.parseplatform.org/parse-server/guide/#requiresauthentication
+ * 
+ * @param {String} schemaResourceUrl The URL pointing to the schema resource, usually it is http(s)://${host}/parse/schemas/${className}
+ * @param {String} appId The app id
+ * @param {String} masterKey The master key
+ * @return {Promise} Promise that represents the request to set CLPs.
+ * @private
+ */
+function setSchemaPermissions(schemaResourceUrl, appId, masterKey)
+{
+  var clp = {
+    "classLevelPermissions": {
+      "get": {
+        "role:Viewer": true
+      },
+      "find": {
+        "role:Viewer": true
+      },
+      "create": {
+        "role:Writer": true
+      },
+      "update": {
+        "role:Writer": true
+      },
+      "delete": {
+        "role:Writer": true
+      },
+      "addField": {
+        // Add this if we need to allow dynamic field creation.
+        // "role:Writer": true
+      }
+    },
+  };
+
+  return axios.put(schemaResourceUrl, JSON.stringify(clp), {headers: {
+    'Content-Type': "application/json",
+    'X-Parse-Application-Id': appId,
+    'X-Parse-Master-Key': masterKey,
+    }});
+}
+
+/**
+ * This function is the entry point to initialize all AHOPE application-specific database structures,
+ * namely, classes (schemas), roles, and their permissions.
+ * 
+ * @param {*} serverConfig The configuration about the Parse server, including app id, master key, and to a lesser degree of usefulness,
+ * the public URL of the Parse server.
+ * 
+ * @param {*} parseLocalUrl An optional parameter, when present, override serverConfig.serverURL.  The typical use case is to use a
+ * localhost based URL, rather than the public Parse server URL to access the Parse server.  The significance here is not performance,
+ * but rather to ensure the initialization code here, in possession of the master key that is only guaranteed to be valid for the Parse
+ * server in the same GCP AppEngine Service instance, is authenticated.  If the public URL were used, REST requests may be routed to 
+ * other instances, and (at the time of writing) the master key may not work there.
+ */
+function setParseSchema (serverConfig, parseLocalUrl) {
 
   Parse.initialize(serverConfig.appId, null, serverConfig.masterKey);
-  Parse.serverURL = serverConfig.serverURL ;
+  Parse.serverURL = parseLocalUrl || serverConfig.serverURL ;
+
+  // We are going to create four roles for AHOPE classes:
+  // (1) a Reader can read, but not write.
+  // (2) a Writer can write, but not read.
+  // (3) an Editor is a member of Reader and Writer roles, so can both read and write.
+  // (4) an Administrator can do anything.  At this time, it is the same as Editor.
+  // We recommend that the Administrator (who can access the dashboard) only grant Reader and Editor roles
+  // to staff members at this time.
+
+  var allPromises = [];
+
+  var adminRole, editorRole;
+
+  const promiseToCreateAllRoles = createRole("Administrator")
+    .then(role => createRole("Editor", adminRole = role))
+    .then(role => createRole("Writer", editorRole = role))
+    .then(() => createRole("Viewer", editorRole));
+
+  allPromises.push(promiseToCreateAllRoles);
   
 /*
   Notes:
 
   (1) A Parse.Schema only represents data needed to make a request about a Parse Schema.  A better name would be SchemaRequest.
   
-  (2) As such, fields added by Parse.Schema.prototype.addField() function and its ilks only represent intention to add/create them, and nothing else.
-      For example, the .get() method doesn't change any such "fields" in the Schema object based on the result returned from the Parse server.
+  (2) As such, fields added by Parse.Schema.prototype.addField() function and its ilks only represent intention to add/create them, and nothing more.
+      For example, the .get() method doesn't update (synchronize) any such "fields" in the in-memory Schema object, based on the result returned from 
+      the Parse server.
   
   (3) Starting from Parse v2, standard Javascript Promise has replaced Parse.Promise, which unfortunately is still used in sample code
       in ParsePlatform documentation as of this writing.
+
+  (4) It might be noted by observant developers/reviewers that in the code below, save()/update()/get() calls are made without an "options" parameter
+      specifying an authentication mode.  This is because, according to https://docs.parseplatform.org/js/guide/#schema, Schema is special in that
+      Master Key is always used, and hence such an options parameter is not useful.  This is in contrast to other classes such as Role, where a 
+      request method (i.e., a method that causes a REST request to be sent to Parse server) if invoked without option seems to be interpreted as
+      accessing as an unauthenticated user (i.e. public).
  */
   const contactSchema = new Parse.Schema('contacts');
 
-  contactSchema
+  const promiseToCreateContactsClass = contactSchema
     // fields are listed by type and in alphabetical order.  In dashboard you may move columns around to examine.
     .addArray('otherDrugs')
     .addArray('otherDrugsAggregate')
@@ -68,9 +186,11 @@ function setParseSchema (serverConfig) {
     .then(existingSchemaData => addAnyNewFieldsToSchema(existingSchemaData, contactSchema))
     .catch(error => ((error.code === Parse.Error.INVALID_CLASS_NAME) ? contactSchema.save() : Promise.reject(error)));
 
+  allPromises.push(promiseToCreateContactsClass);
+
   const eventSchema = new Parse.Schema('event');
 
-  eventSchema
+  const promiseToCreateEventClass = eventSchema
     // fields are listed by type and in alphabetical order.  In dashboard you may move columns around to examine.
     .addArray('otherDrugs')
     .addArray('referrals')
@@ -111,6 +231,20 @@ function setParseSchema (serverConfig) {
     .get()
     .then(existingSchemaData => addAnyNewFieldsToSchema(existingSchemaData, eventSchema))
     .catch(error => ((error.code === Parse.Error.INVALID_CLASS_NAME) ? eventSchema.save() : Promise.reject(error)));
+
+  allPromises.push(promiseToCreateEventClass);
+
+  return Promise.all(allPromises)
+    .then(() => {
+      return setSchemaPermissions(Parse.serverURL + "/schemas/contacts", serverConfig.appId, serverConfig.masterKey);
+    })
+    .then(() => {
+      return setSchemaPermissions(Parse.serverURL + "/schemas/event", serverConfig.appId, serverConfig.masterKey);
+    })
+    .catch(error => {
+      console.log(error);
+      return Promise.reject(error);
+    })
 }
 
 module.exports = setParseSchema;
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const path = require('path');
 const yml = require('js-yaml');
 const ymlFormatter = { parse: yml.safeLoad, stringify: yml.safeDump };
 
-var projectId, connectionName, databaseName;
+var connectionName, databaseName;
 
 if (process.env.NODE_ENV === 'production') {
   require('@google-cloud/debug-agent').start();
@@ -35,6 +35,25 @@ nconf.argv().env()
 
 const app = express();
 
+const projectId = process.env.GOOGLE_CLOUD_PROJECT || nconf.get('PROJECT_ID');
+
+let masterKey = nconf.get('MASTER_KEY');
+let crypto;
+
+if (!masterKey) {
+  if (!crypto) crypto = require('crypto');
+  if (process.env.GAE_VERSION && process.env.MK_SALT) {
+    // this will generate a master key specific to a version, but shared by multiple instances of a version.
+    // assuming process.env.MK_SALT is version specific (which should be as it is generated at build-time).
+
+    // scryptSync() is unavailable on node 8.
+    // masterKey = crypto.scryptSync(process.env.GAE_VERSION, process.env.MK_SALT, 64, {N: 1024}).toString('base64');
+    masterKey = crypto.createHmac('sha256', process.env.MK_SALT).update(process.env.GAE_VERSION).digest('base64');
+  } else {
+    masterKey = crypto.randomBytes(48).toString('base64');
+  }
+}
+
 // Reference: https://parseplatform.org/parse-server/api/master/ParseServerOptions.html
 const serverConfig = {
   cloud: nconf.get('CLOUD_PATH') || path.join(__dirname, '/cloud/main.js'),
@@ -42,7 +61,7 @@ const serverConfig = {
   // we are only deploying one app.
   appId: nconf.get('APP_ID') || 'AHOPE-App-in-Parse',
   // An empty or undefined master key indicates that Parse server and Parse Dashboard are collocated and can use a randomly generated password
-  masterKey: nconf.get('MASTER_KEY') || require('crypto').randomBytes(48).toString('base64'),
+  masterKey: masterKey,
   fileKey: nconf.get('FILE_KEY'),
   mountPath: nconf.get('PARSE_MOUNT_PATH') || '/parse',
   auth: { google: true },
@@ -50,15 +69,21 @@ const serverConfig = {
   allowClientClassCreation: nconf.get('ALLOW_CLIENT_CLASS_CREATION') || false
 }
 
+serverConfig.serverURL = nconf.get('SERVER_URL') || 'https://'.concat(projectId, '.appspot.com', serverConfig.mountPath);
 
-serverConfig.serverURL = nconf.get('SERVER_URL') || 'https://'.concat(projectId = nconf.get('PROJECT_ID'), '.appspot.com', serverConfig.mountPath),
+// Generate a version-specific URL to be used by the Dashboard, so that it targets the version where it is from.
+// See https://cloud.google.com/appengine/docs/standard/java/how-requests-are-routed#targeted_routing
+const serviceId = process.env.GAE_SERVICE || 'default';   // the id of the AppEngine service this piece of code is running in.
+const versionSpecificServerUrl = nconf.get('SERVER_URL') || (process.env.GAE_VERSION ? 'https://'.concat(process.env.GAE_VERSION, '-dot-', serviceId, '-dot-', projectId, '.appspot.com', serverConfig.mountPath) : serverConfig.serverURL);
+
+console.info(`Parse Server URL to be used by Dashboard is: ${versionSpecificServerUrl}`);
 
 serverConfig.databaseURI = nconf.get('DATABASE_URI');
 
 function getConnectionName()
 {
   return nconf.get('CLOUDSQL_CONNECTION_NAME') || 
-    [(projectId || nconf.get('PROJECT_ID')), nconf.get('CLOUDSQL_REGION'), nconf.get('CLOUDSQL_INSTANCE_ID')].join(':')
+    [projectId, nconf.get('CLOUDSQL_REGION'), nconf.get('CLOUDSQL_INSTANCE_ID')].join(':') ;
 }
 
 if (!serverConfig.databaseURI) {
@@ -112,19 +137,60 @@ app.get('/dummy.js', function (req, res) {
   res.type('text/javascript').send("console.log('hello from the dummy service worker');");
 })
 
+const PORT = process.env.PORT || 8080;
+const PARSE_MOUNT_PATH = serverConfig.mountPath || '/parse';
+
 // Note: this endpoint triggers AHOPE application-specific initialization.
 // The reason why "/parse/ahopeinit" is selected is that in the front-end code, we already
 // allow requests to /parse/* and /dashboard/* to not to be intercepted by the service-worker.
 // If we choose to use "/ahopeinit", then we need to change the front-end code.
-app.get('/parse/ahopeinit', function (req, res) {
+app.post(PARSE_MOUNT_PATH + '/ahopeinit', function (req, res) {
   console.log("Request to initialize has been received.");
-  res.type('text/plain').send("Schema initialization will start.");
+
+  // If we are running in production (i.e., process.env.MK_SALT is set), we want to
+  // prevent any unauthorized actor to request a schema init.
+  // We require an authorization header be present, with Basic as the authentication scheme,
+  // and the password part being the SHA2 hash to MK_SALT (the GCP cloud builder generates
+  // it and therefore is in possession of it).
+  //
+  if (process.env.MK_SALT) {    
+    let authenticated = false ;
+
+    if (req.headers.authorization && req.headers.authorization.toLowerCase().startsWith('basic ')) {
+      const authString = new Buffer(req.headers.authorization.split(" ")[1], 'base64').toString();
+      console.log("Authorization header detected: " + authString);
+      const secretSplit = authString.split(":")
+      if (secretSplit.length > 1)
+      {
+        const secret = secretSplit[1];
+        if (!crypto) crypto = require('crypto');
+        const hash = crypto.createHash('sha256');
+        const expectedSecret = hash.update(process.env.MK_SALT).digest('hex');
+        authenticated = (expectedSecret === secret) ;
+      }  
+    }
+
+    if (false == authenticated) {
+      console.info("Request to initialize schema is rejected as unauthorized.");
+      res.status(401).type('text/plain').send("Unauthorized.");
+      return;
+    }
+  }
+
+  // res.type('text/plain').send("Schema initialization will start.");
   const setParseSchema = require('./parseSchema');
-  setParseSchema(serverConfig);
+  const parseLocalUrl = "http://localhost:" + PORT + PARSE_MOUNT_PATH;
+  setParseSchema(serverConfig, parseLocalUrl)
+  .then(() => {
+    res.type('text/plain').send("Schema initialization completed.");
+  })
+  .catch(() => {
+    res.status(500).type('text/plain').send("Schema initialization failed.");
+  });
 });
 
 // Mount the Parse API server middleware to /parse
-app.use(serverConfig.mountPath || '/parse', parseServer);
+app.use(PARSE_MOUNT_PATH, parseServer);
 
 const dashboardSettings = nconf.get('DASHBOARD_SETTINGS');
 
@@ -133,7 +199,7 @@ if (dashboardSettings) {
 
   const appSettings = dashboardSettings.apps[0];
 
-  if (!appSettings.serverURL) appSettings.serverURL = serverConfig.serverURL;
+  if (!appSettings.serverURL) appSettings.serverURL = versionSpecificServerUrl; // serverConfig.serverURL;
   if (!appSettings.appId) appSettings.appId = serverConfig.appId;
   if (!appSettings.masterKey) appSettings.masterKey = serverConfig.masterKey;
   if (!appSettings.appName) appSettings.appName = "AHOPE Administrators Dashboard";
@@ -173,7 +239,6 @@ app.get('/', (req, res) => {
 // they should not be "masked" by the front end resources.
 app.use(express.static('front-end'));
 
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
